dcb349ad2c5624a33765f5da124c3abd23084f77
PcDiego <DAlonsoFeijoo26@gmail.com>
2026-07-24 10:00:02 +0200

    Añadir estructura basica de carpetas, enrutamiento con react-router y crear páginas de inicio y error 404

---
+++ oisterWeb/src/pages/404.jsx
@@ -0,0 +1,10 @@
+function NotFoundPage() {
+    return (
+        <div>
+            <h1>ERROR 404</h1>
+            <p>ERROR ERROR CAMBIAR ESTO </p>
+        </div>
+    );
+}
+
+export default NotFoundPage;
---
+++ oisterWeb/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+function HomePage() {
+    return (
+        <div>
+            <h1>Pagina principal</h1>
+            <p>This is the main content of the home page.</p>
+        </div>
+    );
+}
+
+export default HomePage;
